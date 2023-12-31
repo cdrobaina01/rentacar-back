4757fbb06e075e5d8e9945e39ee0159702cf54bc
Carlos Daniel Robaina Rivero <cdrobayna01@gmail.com>
2023-12-30 23:57:56 -0500

    fix: import & security extras

--- src/main/java/cu/edu/cujae/rentacarback/controller/AuthController.java
+++ src/main/java/cu/edu/cujae/rentacarback/controller/AuthController.java
@@ -1,22 +1,13 @@
 package cu.edu.cujae.rentacarback.controller;
 
-import cu.edu.cujae.rentacarback.security.UserPrincipal;
 import cu.edu.cujae.rentacarback.security.dto.LoginRequestDTO;
 import cu.edu.cujae.rentacarback.security.dto.LoginResponseDTO;
-import cu.edu.cujae.rentacarback.security.jwt.JwtIssuer;
 import cu.edu.cujae.rentacarback.service.core.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
